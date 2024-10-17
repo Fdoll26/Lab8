fd5b9c66cd268300095c23914ad7277482e7d5ac
Francis Doll <fdoll@ualberta>
2024-10-17 14:30:04 -0600

    Add City Test Failure

--- app/src/androidTest/java/com/example/lab8/CustomListTest.java
+++ app/src/androidTest/java/com/example/lab8/CustomListTest.java
@@ -8,6 +8,8 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.junit.Assert.assertEquals;
+
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -16,26 +18,23 @@ import androidx.test.filters.LargeTest;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import com.example.lab8.CustomList;
+
+import java.util.ArrayList;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 
-public class MainActivityTest {
-    @Rule
-    public ActivityScenarioRule<MainActivity> scenario = new
-            ActivityScenarioRule<MainActivity>(MainActivity.class);
+public class CustomListTest {
+//    @Rule
+//    public ActivityScenarioRule<MainActivity> scenario = new
+//            ActivityScenarioRule<MainActivity>(MainActivity.class);
 
 
     @Test
     public void testAddContent(){
-// Click on Add City button
-        onView(withId(R.id.button_add)).perform(click());
-// Type "Edmonton" in the editText
-        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
-// Click on Confirm
-        onView(withId(R.id.button_confirm)).perform(click());
-// Check if text "Edmonton" is matched with any of the text
-//        displayed on the screen
-        onView(withText("Edmonton")).check(matches(isDisplayed()));
+        CustomList testList = null;
+        testList.addCity(new City("Winnipeg","MN"));
+        assertEquals(testList.getCount(), 1);
     }
 }
