ab284b6e19f2967111e121db6a4f4b8e61113181
Francis Doll <fdoll@ualberta>
2024-10-23 17:15:22 -0600

    Customs Test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -1,16 +1,30 @@
 package com.example.lab8;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
-//import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 
+import static org.junit.Assert.assertEquals;
+
 public class CustomListTest {
 
+    private CustomList list;
 
+    @Before
+    public void setUp() {
+        list = new CustomList(null, new ArrayList<>());
+    }
+
+    @Test
+    public void dummyTest() {
+        assertEquals(1, 2); // This should fail
+    }
+
+    @Test
+    public void testAddCityTest() {
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(), listSize + 1);
+    }
 }
